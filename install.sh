688c9f65f01f41b8f3fdb1eaa8fa4c27cb567319
Tavian Barnes <tavianator@tavianator.com>
2014-10-09 11:38:51 -0400

    gcc: Actually use hard floats.

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ fi
 arch="$(uname -m)"
 prefix=arm-linux-gnueabihf
 binutils_version=2.24-1.1-${arch}
-gcc_stage1_version=4.9.1-2-${arch}
+gcc_stage1_version=4.9.1-2.1-${arch}
 linux_api_headers_version=3.16.1-1-any
 glibc_headers_version=2.19-1.1-any
-gcc_stage2_version=4.9.1-2-${arch}
+gcc_stage2_version=4.9.1-2.1-${arch}
 glibc_version=2.19-1-any
-gcc_version=4.9.1-2-${arch}
+gcc_version=4.9.1-2.1-${arch}
 
 gcc_filename="gcc-${gcc_version%%-*}.tar.bz2"
 glibc_filename="glibc-${glibc_version%%-*}.tar.bz2"

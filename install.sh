15cbfb1c9668d2a2dacd88a12bb9f5f0ae660dce
Tavian Barnes <tavianator@tavianator.com>
2015-09-13 17:37:04 -0400

    Update to latest versions.

--- install.sh
+++ install.sh
@@ -2,31 +2,15 @@
 
 set -e
 
-force=
-if [ "$1" = "-f" ]; then
-    force="-f"
-fi
+prefix="arm-linux-gnueabihf"
+gcc_filename="gcc-5.2.0.tar.bz2"
+isl_filename="isl-0.14.1.tar.bz2"
+glibc_filename="glibc-2.22.tar.bz2"
 
-arch="$(uname -m)"
-prefix=arm-linux-gnueabihf
-binutils_version=2.24-1.1-${arch}
-gcc_stage1_version=4.9.2-0.1-${arch}
-linux_api_headers_version=3.16.2-0.1-any
-glibc_headers_version=2.20-0.1-any
-gcc_stage2_version=4.9.2-0.1-${arch}
-glibc_version=2.20-0.1-any
-gcc_version=4.9.2-0.1-${arch}
-
-gcc_filename="gcc-${gcc_version%%-*}.tar.bz2"
-glibc_filename="glibc-${glibc_version%%-*}.tar.bz2"
+args=("$@")
 
 function build() {
-    eval "version=\${${1//-/_}_version}"
-    pkg="${prefix}-$1/${prefix}-$1-${version}.pkg.tar.xz"
-    if [ -n "${force}" -o ! -f "${pkg}" ]; then
-        (cd "./${prefix}-$1" && makepkg "${force}")
-    fi
-    sudo pacman -U "${pkg}"
+    (cd "./${prefix}-$1" && makepkg -i "${args[@]}")
 }
 
 build binutils
@@ -34,10 +18,12 @@ build gcc-stage1
 
 build linux-api-headers
 build glibc-headers
-ln -sf "../${prefix}-gcc-stage1/${gcc_filename}" "${prefix}-gcc-stage2/${gcc_filename}"
+ln -sf "../${prefix}-gcc-stage1/${gcc_filename}" "${prefix}-gcc-stage2/"
+ln -sf "../${prefix}-gcc-stage1/${isl_filename}" "${prefix}-gcc-stage2/"
 build gcc-stage2
 
-ln -sf "../${prefix}-glibc-headers/${glibc_filename}" "${prefix}-glibc/${glibc_filename}"
+ln -sf "../${prefix}-glibc-headers/${glibc_filename}" "${prefix}-glibc/"
 build glibc
-ln -sf "../${prefix}-gcc-stage2/${gcc_filename}" "${prefix}-gcc/${gcc_filename}"
+ln -sf "../${prefix}-gcc-stage2/${gcc_filename}" "${prefix}-gcc/"
+ln -sf "../${prefix}-gcc-stage2/${isl_filename}" "${prefix}-gcc/"
 build gcc

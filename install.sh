e48b7f2253fa4da1ade5901bb274d16cfe5cae68
Tavian Barnes <tavianator@tavianator.com>
2017-02-02 20:17:48 -0500

    Bump to latest

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 set -e
 
 prefix="arm-linux-gnueabihf"
-gcc_filename="1bbd3999.tar.gz"
+gcc_filename="4ca53f06ff7d346ef8021a23108f23a5406a0417.tar.gz"
 isl_filename="isl-0.16.1.tar.bz2"
-glibc_filename="glibc-2.23.tar.xz"
+glibc_filename="glibc-2.24.tar.xz"
 
 args=("$@")
 
@@ -14,16 +14,20 @@ function build() {
 }
 
 build binutils
+
 build gcc-stage1
 
 build linux-api-headers
+
 build glibc-headers
+
 ln -sf "../${prefix}-gcc-stage1/${gcc_filename}" "${prefix}-gcc-stage2/"
 ln -sf "../${prefix}-gcc-stage1/${isl_filename}" "${prefix}-gcc-stage2/"
 build gcc-stage2
 
 ln -sf "../${prefix}-glibc-headers/${glibc_filename}" "${prefix}-glibc/"
 build glibc
+
 ln -sf "../${prefix}-gcc-stage2/${gcc_filename}" "${prefix}-gcc/"
 ln -sf "../${prefix}-gcc-stage2/${isl_filename}" "${prefix}-gcc/"
 build gcc
